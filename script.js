53512d04aa224e3765c47b3279979f890a722ed4
Mishkaag <grigoryewmihailll@gmail.com>
2025-06-18 20:18:22 +0500

    Update script.js
    Удалил блюдо

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
             title: 'ХОЛОДНЫЕ ЗАКУСКИ',
             dishes: [
                 { name: 'Мясное ассорти', price: 15, checked: false },
-                { name: 'Мясное/Сырное ассорти VIP', price: 12, checked: false },
+                { name: 'Удалил блюдо', price: 12, checked: false },
                 { name: 'Сырное канапе', price: 10, checked: false },
                 { name: 'Овощное (садака)', price: 8, checked: false },
                 { name: 'Овощное (банкет)', price: 10, checked: false },
